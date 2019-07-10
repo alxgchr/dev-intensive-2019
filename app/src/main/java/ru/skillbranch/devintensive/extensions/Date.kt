a18711112880fd22f595bd84eb29fd4f5fd9e36c
Alexey <alxg.chr@gmail.com>
2019-07-10 10:56:55 +0200

    hometask_2 ver 4

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -31,22 +31,43 @@ fun Date.add(value: Int, timeUnits: TimeUnits = TimeUnits.SECOND): Date {
     return this
 }
 
+//enum class TimeUnits {
+//    SECOND {
+//        override fun plural(num: Int): String {
+//            return "${if(num!=1) "$num " else ""}${if (num%10 == 1) "секунду" else if (num%10 in 2..4) "секунды" else "секунд"}"
+//        }
+//    }, MINUTE {
+//        override fun plural(num: Int): String {
+//            return "${if(num!=1) "$num " else ""}${if (num%10 == 1) "минуту" else if (num%10 in 2..4) "минуты" else "минут"}"
+//        }
+//    }, HOUR {
+//        override fun plural(num: Int): String {
+//            return "${if(num!=1) "$num " else ""}${if (num%10 == 1) "час" else if (num%10 in 2..4) "часа" else "часов"}"
+//        }
+//    }, DAY {
+//        override fun plural(num: Int): String {
+//            return "${if(num!=1) "$num " else ""}${if (num%10 == 1) "день" else if (num%10 in 2..4) "дня" else "дней"}"
+//        }
+//    };
+//
+//    abstract fun plural(num: Int): String
+//}
 enum class TimeUnits {
     SECOND {
         override fun plural(num: Int): String {
-            return "${if(num!=1) "$num " else ""}${if (num%10 == 1) "секунду" else if (num%10 in 2..4) "секунды" else "секунд"}"
+            return "$num ${if (num%10 == 1) "секунду" else if (num%10 in 2..4) "секунды" else "секунд"}"
         }
     }, MINUTE {
         override fun plural(num: Int): String {
-            return "${if(num!=1) "$num " else ""}${if (num%10 == 1) "минуту" else if (num%10 in 2..4) "минуты" else "минут"}"
+            return "$num ${if (num%10 == 1) "минуту" else if (num%10 in 2..4) "минуты" else "минут"}"
         }
     }, HOUR {
         override fun plural(num: Int): String {
-            return "${if(num!=1) "$num " else ""}${if (num%10 == 1) "час" else if (num%10 in 2..4) "часа" else "часов"}"
+            return "$num ${if (num%10 == 1) "час" else if (num%10 in 2..4) "часа" else "часов"}"
         }
     }, DAY {
         override fun plural(num: Int): String {
-            return "${if(num!=1) "$num " else ""}${if (num%10 == 1) "день" else if (num%10 in 2..4) "дня" else "дней"}"
+            return "$num ${if (num%10 == 1) "день" else if (num%10 in 2..4) "дня" else "дней"}"
         }
     };
 
@@ -68,11 +89,13 @@ fun Date.humanizeDiff(date: Date = Date()): String {
 
         interval / SECOND <= 1 -> "только что"
         interval / SECOND <= 45 -> if(isFuture) {"через несколько секунд"} else{"несколько секунд назад"}
-        //interval / SECOND <= 75 -> if(isFuture) {"через минуту"} else{"минуту назад"}
+        interval / SECOND <= 75 -> if(isFuture) {"через минуту"} else{"минуту назад"}
 
         interval / MINUTE <=45 -> if(isFuture) {"через ${TimeUnits.MINUTE.plural((interval/ MINUTE).toInt())}"} else{"${TimeUnits.MINUTE.plural((interval/ MINUTE).toInt())} назад"}
-        interval / HOUR <=22 -> if(isFuture) {"через ${TimeUnits.MINUTE.plural((interval/ HOUR).toInt())}"} else{"${TimeUnits.HOUR.plural((interval/ HOUR).toInt())} назад"}
+        interval / MINUTE <=75 -> if(isFuture) {"через час"} else{"час назад"}
+       // interval / HOUR <=22 -> if(isFuture) {"через ${TimeUnits.MINUTE.plural((interval/ HOUR).toInt())}"} else{"${TimeUnits.HOUR.plural((interval/ HOUR).toInt())} назад"}
         interval / HOUR <=22 -> if(isFuture) {"через ${TimeUnits.HOUR.plural((interval/ HOUR).toInt())}"} else{"${TimeUnits.HOUR.plural((interval/ HOUR).toInt())} назад"}
+        interval / HOUR <=26 -> if(isFuture) {"через день"} else{"день назад"}
         interval / DAY <=360 -> if(isFuture) {"через ${TimeUnits.DAY.plural((interval/ DAY).toInt())}"} else{"${TimeUnits.DAY.plural((interval/ DAY).toInt())} назад"}
         else-> if(isFuture) {"более чем через год"} else{"более года назад"}
     }
