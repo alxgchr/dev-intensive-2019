d74f701ee500286043e16ae96223d178d4013159
Alexey <alxg.chr@gmail.com>
2019-09-30 23:14:11 +0200

    hometask_4 ver 3

--- app/src/main/java/ru/skillbranch/devintensive/App.kt
+++ app/src/main/java/ru/skillbranch/devintensive/App.kt
@@ -1,7 +1,31 @@
 package ru.skillbranch.devintensive
 
+import android.app.Application
+import android.content.Context
+import androidx.appcompat.app.AppCompatDelegate
+import ru.skillbranch.devintensive.repositories.PreferencesRepository
+
 /**
  * Created by Alexey on 29.09.2019
  */
-class App {
+class App : Application() {
+
+    companion object {
+        private var instance: App? = null
+
+        fun applicationContext(): Context {
+            return instance!!.applicationContext
+        }
+    }
+
+    init {
+        instance = this
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        PreferencesRepository.getAppTheme().also {
+            AppCompatDelegate.setDefaultNightMode(it)
+        }
+    }
 }
--- app/src/main/java/ru/skillbranch/devintensive/models/Profile.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Profile.kt
@@ -3,5 +3,25 @@ package ru.skillbranch.devintensive.models
 /**
  * Created by Alexey on 29,Сентябрь,2019
  */
-class Profile {
+data class Profile(
+    val firstName: String,
+    val lastName: String,
+    val about: String,
+    val repository: String,
+    val rating: Int = 0,
+    val respect: Int = 0
+) {
+    val nickName: String = "J D"
+    val rank: String = "Junior Android Developer"
+
+    fun toMap(): Map<String, Any> = mapOf(
+        "nickName" to nickName,
+        "rank" to rank,
+        "firstName" to firstName,
+        "lastName" to lastName,
+        "about" to about,
+        "repository" to repository,
+        "rating" to rating,
+        "respect" to respect
+    )
 }
